ce5352495fd9ca54d306f5d036607ffb1a182149
Matt Lajoie <mlajoie03@gmail.com>
2018-02-18 10:36:22 -0500

    demo init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppComponent} from './app.component';
+import {MatButtonModule, MatCardModule} from '@angular/material';
 
 
 @NgModule({
@@ -10,7 +11,9 @@ import {AppComponent} from './app.component';
     ],
     imports: [
         BrowserModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatButtonModule
     ],
     providers: [],
     bootstrap: [AppComponent]
